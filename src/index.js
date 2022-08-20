1b2af150b4a63aa751f904b6b3644627560d01b7
Kate Sidoriuk <CCEBEPA@yandex.ru>
2022-08-20 19:27:32 +0200

    Добавлен второй вариант шапки для десктопа, верстка меню мобильной версии и изменение шапки при скролле.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './pages/index.css';
+
+const headerStart = document.querySelector('.header__start');
+const headerScroll = document.querySelector('.header__scroll');
+
+console.log(123);
+
+let last_known_scroll_position = 0;
+
+function addScrollListener() {
+window.addEventListener('scroll', function(e){
+if(last_known_scroll_position == 0 && window.scrollY !== 0) {
+	headerStart.style.display = 'none';
+	headerScroll.style.display = 'flex';
+} if (last_known_scroll_position !== 0 && window.scrollY == 0) {
+	headerStart.style.display = 'flex';
+	headerScroll.style.display = 'none';
+}
+ last_known_scroll_position = window.scrollY;
+})
+}
+addScrollListener();
