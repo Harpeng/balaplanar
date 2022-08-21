f72b3a7576b5e3ec1c8058a373f8827b35abcd1d
Kate Sidoriuk <CCEBEPA@yandex.ru>
2022-08-21 15:16:44 +0200

    Добавлен js для вывода меню в мобильной версии.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,38 @@ import './pages/index.css';
 
 const headerStart = document.querySelector('.header__start');
 const headerScroll = document.querySelector('.header__scroll');
+const buttonMenuStart = headerStart.querySelector('.header__menu');
+const buttonMenuScroll = headerScroll.querySelector('.header__menu');
+const headerMobile = document.querySelector('.header__mobile');
+const buttonExit = document.querySelector('.button-exit');
 
-console.log(123);
 
 let last_known_scroll_position = 0;
 
 function addScrollListener() {
-window.addEventListener('scroll', function(e){
-if(last_known_scroll_position == 0 && window.scrollY !== 0) {
-	headerStart.style.display = 'none';
-	headerScroll.style.display = 'flex';
-} if (last_known_scroll_position !== 0 && window.scrollY == 0) {
-	headerStart.style.display = 'flex';
-	headerScroll.style.display = 'none';
-}
- last_known_scroll_position = window.scrollY;
-})
+	window.addEventListener('scroll', function (e) {
+		if (last_known_scroll_position == 0 && window.scrollY !== 0) {
+			headerStart.style.display = 'none';
+			headerScroll.style.display = 'flex';
+		} if (last_known_scroll_position !== 0 && window.scrollY == 0) {
+			headerStart.style.display = 'flex';
+			headerScroll.style.display = 'none';
+		}
+		last_known_scroll_position = window.scrollY;
+	})
 }
 addScrollListener();
+
+buttonMenuStart.addEventListener('click', function (e) {
+	console.log(123)
+	headerMobile.style.display = 'flex';
+})
+
+buttonMenuScroll.addEventListener('click', function (e) {
+	console.log(123)
+	headerMobile.style.display = 'flex';
+})
+
+buttonExit.addEventListener('click', function (e) {
+	headerMobile.style.display = 'none';
+})
