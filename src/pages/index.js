ec5989de97e10046d26c804b992d9b0bb9f9f073
trqktop <trqktop@yandex.ru>
2022-08-21 23:34:03 +0300

    feat/mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,31 @@
 import './index.css';
+import {
+    container, scrollElements, scrollElement
+} from '../utils/constants.js'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
-console.log(ScrollTrigger)
+
+
+
+
+if (document.documentElement.clientWidth > 1000) {
+    gsap.to(scrollElements, {
+        xPercent: -100 * (scrollElements.length - 1),
+        // ease: "none",
+        scrollTrigger: {
+            trigger: container,
+            pin: true,
+            start: "center center",
+            scrub: 0.5,
+            markers: true,
+            toggleClass: { className: 'principles__scroll-container_move', targets: container },
+            end: () => `+=${container.offsetHeight * (scrollElements.length - 1)}`
+        }
+    });
+
+}
+
+
+
+
