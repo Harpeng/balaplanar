d99851a48cf09a7422e41c3d4adef6477bb80bf5
Nastasia <lisitsinan@gmail.com>
2022-08-22 20:00:56 +0300

    JS для модальных окон

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
 import "./index.css";
+import { modalConfig } from '../components/constants';
+import { openPopup, closePopupByOverlayAndIcon } from '../components/popup';
+
+const courseReviewBtn = document.querySelector('.course__review-button');
+const courseReviewPopup = document.querySelector('.popup__course');
+
+courseReviewBtn.addEventListener('click', () => openPopup(courseReviewPopup));
+
+const popups = document.querySelectorAll(modalConfig.modalSelector);
+
+popups.forEach((popup) => {
+	popup.addEventListener('click', closePopupByOverlayAndIcon);
+});
