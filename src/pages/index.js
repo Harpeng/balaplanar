290ea08e745a9a1f2d82bbd16b9bb86bfe502e39
Kate Sidoriuk <CCEBEPA@yandex.ru>
2022-08-24 19:48:23 +0200

    Исправлены кнопки, добавлены якори

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import {
-    container, scrollElements, scrollElement
+	container, scrollElements, scrollElement
 } from '../utils/constants.js'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -13,19 +13,19 @@ let principlesSection = document.querySelector('.principles')
 gsap.registerPlugin(ScrollTrigger);
 
 if (document.documentElement.clientWidth > 1024) {
-    gsap.to(scrollElements, {
-        xPercent: -100 * (scrollElements.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: container,
-            pin: true,
-            pinType: 'transform',
-            start: "center center",
-            scrub: 0.5,
-            toggleClass: { className: 'principles__scroll-container_move', targets: container },
-            end: () => `+=${container.offsetHeight * (scrollElements.length - 1)}`
-        }
-    });
+	gsap.to(scrollElements, {
+		xPercent: -100 * (scrollElements.length - 1),
+		ease: "none",
+		scrollTrigger: {
+			trigger: container,
+			pin: true,
+			pinType: 'transform',
+			start: "center center",
+			scrub: 0.5,
+			toggleClass: { className: 'principles__scroll-container_move', targets: container },
+			end: () => `+=${container.offsetHeight * (scrollElements.length - 1)}`
+		}
+	});
 }
 
 // popup
@@ -41,9 +41,9 @@ const partnerOgPopup = document.querySelector('#partners__logo_og-popup');
 const partnerHbPopup = document.querySelector('#partners__logo_hb-popup');
 
 
-partnerLwbLogo.addEventListener('click', () => { openPopup(partnerLwbPopup)});
-partnerOgLogo.addEventListener('click', () => { openPopup(partnerOgPopup)});
-partnerHbLogo.addEventListener('click', () => { openPopup(partnerHbPopup)});
+partnerLwbLogo.addEventListener('click', () => { openPopup(partnerLwbPopup) });
+partnerOgLogo.addEventListener('click', () => { openPopup(partnerOgPopup) });
+partnerHbLogo.addEventListener('click', () => { openPopup(partnerHbPopup) });
 
 const popups = document.querySelectorAll(modalConfig.modalSelector);
 
@@ -62,6 +62,12 @@ const buttonMenuScroll = headerScroll.querySelector('.header__menu');
 const headerMobile = document.querySelector('.header__mobile');
 const buttonExit = document.querySelector('.button-exit');
 
+const menuCloseEsc = (evt) => {
+	if (evt.key == "Escape") {
+		closeMenuMobile();
+	};
+};
+
 
 let last_known_scroll_position = 0;
 
@@ -79,14 +85,21 @@ function addScrollListener() {
 }
 addScrollListener();
 
-buttonMenuStart.addEventListener('click', function (e) {
+buttonMenuStart.addEventListener('click', (e) => {
 	headerMobile.style.top = 0;
+	document.addEventListener('keydown', menuCloseEsc);
 })
 
-buttonMenuScroll.addEventListener('click', function (e) {
+buttonMenuScroll.addEventListener('click', (e) => {
 	headerMobile.style.top = 0;
+	document.addEventListener('keydown', menuCloseEsc);
 })
 
-buttonExit.addEventListener('click', function (e) {
+buttonExit.addEventListener('click', (e) => {
+	closeMenuMobile();
+})
+
+function closeMenuMobile() {
 	headerMobile.style.top = '-100%';
-})
+	document.removeEventListener('keydown', menuCloseEsc);
+}
