b872961a19a0d06183c9841e4795e5eb6d9b3e38
trqktop <trqktop@yandex.ru>
2022-08-26 00:15:15 +0300

    fix/fin

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,105 @@ import { openPopup, closePopupByOverlayAndIcon } from '../components/popup';
 gsap.registerPlugin(ScrollTrigger);
 
 if (document.documentElement.clientWidth > 1024) {
+
+
 	gsap.to(scrollElements, {
-			xPercent: -100 * (scrollElements.length - 1),
-			ease: "none",
-			scrollTrigger: {
-					trigger: container,
-					pin: true,
-					pinType: 'fixed',
-					start: "center center",
-					scrub: 0.5,
-					toggleClass: { className: 'principles__scroll-container_move', targets: container },
-					end: () => `+=${container.offsetHeight * (scrollElements.length - 1)}`
-			}
+		xPercent: -100 * (scrollElements.length - 1),
+		ease: "none",
+		scrollTrigger: {
+			trigger: container,
+			pin: true,
+			pinType: 'fixed',
+			start: 'top top',
+			scrub: 0.5,
+			onToggle: () => {
+				if (innerWidth >= 1440) {
+					gsap.to(
+						container, {
+						ease: "none",
+						delay: 0,
+						duration: 0,
+						overflow: 'visible'
+					})
+					gsap.to(
+						scrollElements, {
+						ease: 'none',
+						delay: 0,
+						duration: 0,
+						backgroundColor: '#AE46FF',
+					})
+				}
+			},
+			onEnter: () => {
+				gsap.to('.header__scroll', {
+					scale: 0.95,
+					borderRadius: '66px',
+					ease: "none",
+					delay: 0,
+					duration: 0,
+				})
+				gsap.to(
+					[container, ...scrollElements], {
+					ease: 'none',
+					delay: 0,
+					duration: 0,
+					borderRadius: '0',
+				})
+			},
+			onEnterBack: () => {
+				gsap.to('.header__scroll', {
+					scale: 0.95,
+					borderRadius: '66px',
+					ease: "none",
+					delay: 0,
+					duration: 0,
+				})
+				gsap.to(
+					[container, ...scrollElements], {
+					ease: 'none',
+					delay: 0,
+					duration: 1,
+					borderRadius: '0',
+				})
+			},
+			onLeave: () => {
+				gsap.to('.header__scroll', {
+					scale: 1,
+					borderRadius: '',
+					ease: "none",
+					delay: 0,
+					duration: 0,
+				})
+				gsap.to(
+					[container, ...scrollElements], {
+					ease: 'none',
+					delay: 0,
+					duration: 1,
+					borderRadius: '60px',
+				})
+			},
+			onLeaveBack: () => {
+				gsap.to('.header__scroll', {
+					scale: 1,
+					borderRadius: '',
+					ease: "none",
+					delay: 0,
+					duration: 0,
+				})
+				gsap.to(
+					[container, ...scrollElements], {
+					ease: 'none',
+					delay: 0,
+					duration: 0,
+					borderRadius: '60px'
+				})
+			},
+			end: () => `+=${container.offsetHeight * (scrollElements.length - 1)}`
+		}
 	})
 }
+
+
 
 // popup
 const courseReviewBtn = document.querySelector('.course__review-button');
@@ -45,14 +130,14 @@ const popups = document.querySelectorAll(modalConfig.modalSelector);
 courseReviewBtn.addEventListener('click', () => openPopup(courseReviewPopup));
 
 popups.forEach((popup) => {
-    popup.addEventListener('click', closePopupByOverlayAndIcon);
+	popup.addEventListener('click', closePopupByOverlayAndIcon);
 });
 
 window.addEventListener('wheel', (e) => {
-    if (e.ctrlKey) {
-        e.preventDefault()
-        return false
-    }
+	if (e.ctrlKey) {
+		e.preventDefault()
+		return false
+	}
 }, { passive: false })
 
 // header menu
